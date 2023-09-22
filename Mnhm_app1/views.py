af8ed2de95d7737f0c3823eeff1b78442b022f03
matias <matias.herrera54@inacapmail.cl>
2023-09-22 12:11:19 -0300

    Se relleno con h1, h2, h3 y botones que lleven para ir navegando por cada vista de manera mas sencilla

--- Mnhm_app1/views.py
+++ Mnhm_app1/views.py
@@ -4,15 +4,33 @@ from django.http import HttpResponse
 def vista_A_Uno(request):
     
     vA="""
-        <h1>a </h1>   
-    
-    """
+        <h1 style="background:yellow">Este es el primero texto de la vista A de la primera app</h1>
+        <br>
+        <br>
+        <h2 style="background:green">Y el siguiente boton</h2>
+        <br>
+        <br>
+        <h3 style="background:darkcyan">Lo llevara a vista B de esta aplicacion 2</h3>
+        <br>
+        <br>
+        <button><a href="../app1_vista2/">Ir a vista B de esta primera app</a></button>
+        <br>
+        <button><a href="../app2_vista1/">Ir a la segunda app </a></button>
+        """
     return HttpResponse(vA)
 
 
 def vista_B_Uno(request):
 
     vB="""
-        <h1>b</h1>
+        <h1 style="background:darkcyan">Ahora esta en la vista B de la primera app</h1>
+        <br>
+        <h2 style="background:yellow"> Con diferentes colores para que se vea la diferencia </h2>
+        <br>
+        <h3 style="background:grey">Si quiere volver a la vista A precione el boton</h3>
+        <br>
+        <button><a href="../app1_vista1/">Ir a vista A de esta primera app</a></button>
+        <br>
+        <button><a href="../app2_vista1/">Ir a la segunda app </a></button>
         """
     return HttpResponse(vB)
--- Mnhm_app2/views.py
+++ Mnhm_app2/views.py
@@ -3,13 +3,31 @@ from django.http import HttpResponse
 
 def vista_A_Dos(request):
     vC="""
-        <h1>c</h1>
+        <h1 style="background:Gray">Este es el primero texto de la vista A de la segunda app</h1>
+        <br>
+        <br>
+        <h2 style="background:green">Y el siguiente boton</h2>
+        <br>
+        <br>
+        <h3 style="background:burlywood">Lo llevara a vista B de esta aplicacion 2</h3>
+        <br>
+        <br>
+        <button><a href="../app2_vista2/">Ir a vista B de la app2</a></button>
+        <br>
+        <button><a href="../app1_vista1/">Ir a la primera app </a></button>
         """
     return HttpResponse(vC)
 
 def vista_B_Dos(request):
     vD="""
-        <h1>d</h1>
-
+        <h1 style="background:brown">Ahora esta en la vista B</h1>
+        <br>
+        <h2 style="background:greenyellow"> Con diferentes colores para que se vea la diferencia </h2>
+        <br>
+        <h3 style="background:gray">Si quiere volver a la vista A precione el boton</h3>
+        <br>
+        <button><a href="../app2_vista1/">Ir a vista A de la app2</a></button>
+        <br>
+        <button><a href="../app1_vista1/">Ir a la primera app </a></button>
         """
     return HttpResponse(vD)
